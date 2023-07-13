4f9da7e0d7a35eef54efab27c896938548a31f97
MyronyukNestor <myronyuk.nestor@gmail.com>
2023-07-13 12:33:05 +0300

    new functionality

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ let price = 2000;
 const titles = document.querySelector(".titles");
 const mainDiv = document.querySelector(".main-div");
 
-// create product cards
+// create product cardss
 
 function createProduct(element, condition) {
   if (condition) {
