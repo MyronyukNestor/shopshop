8e72af2552319b7a650d2e71d4df7b1a7bf41115
MyronyukNestor <myronyuk.nestor@gmail.com>
2023-07-25 12:09:23 +0300

    new functionality

--- slider.js
+++ slider.js
@@ -1,16 +1,16 @@
 const button1 = document.querySelector('.button1');
 const button2 = document.querySelector('.button2');
 const slider = document.querySelectorAll('.slider');
-let currentIndex = 0;
+let index = 0;
 
 button1.addEventListener('click', () => {
-  slider[currentIndex].classList.remove('active');
-  currentIndex = (currentIndex - 1 + slider.length) % slider.length;
-  slider[currentIndex].classList.add('active');
+  slider[index].classList.remove('active');
+  index = (index - 1 + slider.length) % slider.length;
+  slider[index].classList.add('active');
 });
 
 button2.addEventListener('click', () => {
-  slider[currentIndex].classList.remove('active');
-  currentIndex = (currentIndex + 1) % slider.length;
-  slider[currentIndex].classList.add('active');
+  slider[index].classList.remove('active');
+  index = (index + 1) % slider.length;
+  slider[index].classList.add('active');
 });
